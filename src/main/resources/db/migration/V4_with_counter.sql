efce01537878c6385b7fe87397a8c3fc1407ddbb
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-05-13 13:12:22 +0300

    with pgs

--- src/main/resources/db/migration/V4_with_counter.sql
+++
@@ -1,6 +0,0 @@
-select chapter.name,
-       count(card.question) as total_questions,
-       count(card.is_remembered) filter ( where is_remembered=true ) as done_elements
-from chapter
-         left join card on chapter.id = card.chapter_id
-group by chapter.name;
