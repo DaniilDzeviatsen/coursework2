9f7509d3570eca8269baca6b93d0f9ccf9aa18b8
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-05-14 18:22:54 +0300

    with some methods

--- src/main/java/org/example/repositories/ChapterRepositoryImpl.java
+++ src/main/java/org/example/repositories/ChapterRepositoryImpl.java
@@ -19,7 +19,21 @@ public class ChapterRepositoryImpl implements ChapterRepository {
 
     @Override
     public void createNewChapter(String name, long id, long totalCardsNum, long numOfLearnedCards) {
-
+        String sql = """
+                INSERT INTO CHAPTER (NAME)
+                VALUES (?);
+                """;
+        try (
+                Connection connection = db.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+            statement.setString(name),
+                    statement.setLong(id),
+                    statement.setLong(0),
+                    statement.setLong(0);
+        } catch (SQLException e) {
+            throw new RepositoryException(e);
+        }
     }
 
     @Override
@@ -40,35 +54,35 @@ public class ChapterRepositoryImpl implements ChapterRepository {
 
     @Override
     public List<Card> showAllCards(long chapterId) {
-String sql= """
-        SELECT CARD.ID AS ID,
-               CARD.QUESTION AS QUESTION,
-               CARD.ANSWER AS ANSWER,
-               CARD.IS_REMEMBERED AS REMEMBERED
-        FROM CARD
-        WHERE CARD.CHAPTER_ID = ?
-          AND NOT CARD.IS_REMEMBERED
-        ORDER BY CARD.ID
-        LIMIT 1 OFFSET 0;
-        """;
-try(
-        Connection connection= db.getConnection();
-        PreparedStatement statement=connection.prepareStatement(sql);
-        ){
-    statement.setLong(1,chapterId);
-    ResultSet resultSet=statement.executeQuery();
-    List<Card>result = new ArrayList<>();
-    while (resultSet.next()){
-        result.add(new Card(
-                resultSet.getString("question"),
-                resultSet.getString("answer"),
-                resultSet.getBoolean("is_remembered"),
-                resultSet.getLong("id")
-        ));
-    }
-    return result;
-        }catch (SQLException e){
-    throw new RepositoryException(e);
+        String sql = """
+                SELECT CARD.ID AS ID,
+                       CARD.QUESTION AS QUESTION,
+                       CARD.ANSWER AS ANSWER,
+                       CARD.IS_REMEMBERED AS REMEMBERED
+                FROM CARD
+                WHERE CARD.CHAPTER_ID = ?
+                  AND NOT CARD.IS_REMEMBERED
+                ORDER BY CARD.ID
+                LIMIT 1 OFFSET 0;
+                """;
+        try (
+                Connection connection = db.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql);
+        ) {
+            statement.setLong(1, chapterId);
+            ResultSet resultSet = statement.executeQuery();
+            List<Card> result = new ArrayList<>();
+            while (resultSet.next()) {
+                result.add(new Card(
+                        resultSet.getString("question"),
+                        resultSet.getString("answer"),
+                        resultSet.getBoolean("is_remembered"),
+                        resultSet.getLong("id")
+                ));
+            }
+            return result;
+        } catch (SQLException e) {
+            throw new RepositoryException(e);
         }
     }
 
@@ -76,14 +90,14 @@ try(
     @Override
     public List<Chapter> getAllChapters() {
         String sql = """
-               SELECT CHAPTER.NAME AS NAME,
-                                    CHAPTER.ID AS CHAPTER_ID,
-                                    COUNT(CARD.ID) AS TOTAL_QUESTIONS,
-                                    COUNT (CARD.ID) FILTER ( WHERE is_remembered) AS LEARNED_QUESTIONS
-                             FROM CHAPTER
-                                      LEFT JOIN CARD ON CHAPTER.ID = CARD.CHAPTER_ID
-                             GROUP BY CHAPTER.ID;
-                """;
+                SELECT CHAPTER.NAME AS NAME,
+                                     CHAPTER.ID AS CHAPTER_ID,
+                                     COUNT(CARD.ID) AS TOTAL_QUESTIONS,
+                                     COUNT (CARD.ID) FILTER ( WHERE is_remembered) AS LEARNED_QUESTIONS
+                              FROM CHAPTER
+                                       LEFT JOIN CARD ON CHAPTER.ID = CARD.CHAPTER_ID
+                              GROUP BY CHAPTER.ID;
+                 """;
         try (
                 Connection connection = db.getConnection();
                 Statement statement = connection.createStatement();
