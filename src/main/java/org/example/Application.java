7794b084aca1112a171c7dba71d6e78a6137153a
DaniilDzeviatsen <deviat.daan@gmail.com>
2023-05-20 09:59:22 +0300

    with repositories after remarks and hikari

--- src/main/java/org/example/Application.java
+++ src/main/java/org/example/Application.java
@@ -1,25 +1,33 @@
 package org.example;
 
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
 import org.example.repositories.CardRepository;
 import org.example.repositories.CardRepositoryImpl;
 import org.example.repositories.ChapterRepository;
 import org.example.repositories.ChapterRepositoryImpl;
-import org.postgresql.ds.PGSimpleDataSource;
 
 import java.util.Scanner;
 
 
 public class Application {
     public static void main(String[] args) {
+        HikariConfig hikariConfig = new HikariConfig();
+        hikariConfig.setJdbcUrl(System.getenv("FLASHCARDS_DB_URL"));
+        hikariConfig.setUsername(System.getenv("FLASHCARDS_DB_USER"));
+        hikariConfig.setPassword(System.getenv("FLASHCARDS_DB_PASSWORD"));
+        try (HikariDataSource db = new HikariDataSource(hikariConfig)) {
 
-        PGSimpleDataSource db = new PGSimpleDataSource();
+            ChapterRepository chapterRepository = new ChapterRepositoryImpl(db);
+            CardRepository cardRepository = new CardRepositoryImpl(db);
+            Scanner sc = new Scanner(System.in);
+
+        /*PGSimpleDataSource db = new PGSimpleDataSource();
         db.setURL(System.getenv("FLASHCARDS_DB_URL"));
         db.setUser(System.getenv("FLASHCARDS_DB_USER"));
-        db.setPassword(System.getenv("FLASHCARDS_DB_PASSWORD"));
+        db.setPassword(System.getenv("FLASHCARDS_DB_PASSWORD"));*/
 
-        ChapterRepository chapterRepository = new ChapterRepositoryImpl(db);
-        CardRepository cardRepository = new CardRepositoryImpl(db);
-        Scanner sc = new Scanner(System.in);
+
        /* while (true){
             System.out.print("> ");
             String command=sc.nextLine();
@@ -31,12 +39,13 @@ public class Application {
                 }
             }
         }*/
-        //String name=sc.nextLine();
-        //chapterRepository.getAllChapters().stream().forEach(System.out::println);
+            //String name=sc.nextLine();
+            //chapterRepository.getAllChapters().stream().forEach(System.out::println);
 //cardRepository.updateMemorizing(7); ;
-        //List<Chapter> chapters=chapterRepository.getAllChapters();
-        //chapterRepository.getAllChapters().stream().forEach(System.out::println);
-        //cardRepository.showAllCards(3).stream().forEach(System.out::println);
-        cardRepository.getCardsToTraining(3, 1).stream().forEach(System.out::println);
+            //List<Chapter> chapters=chapterRepository.getAllChapters();
+            //chapterRepository.getAllChapters().stream().forEach(System.out::println);
+            //cardRepository.showAllCards(3).stream().forEach(System.out::println);
+            cardRepository.getCardsToTraining(3, 1).stream().forEach(System.out::println);
+        }
     }
 }
